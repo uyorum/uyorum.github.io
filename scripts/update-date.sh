3c2aadf6132274de96ac29ed5d8693dcc6dc2d85
uyorum <uyorum.pub@gmail.com>
2025-02-25 01:50:35 +0900

    Rebuilding site

--- scripts/update-date.sh
+++ scripts/update-date.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git ls-files --others --exclude-standard | grep -P '.*\.md$' | xargs sed -i "s/^date = .*/date = \"$(date --iso-8601=seconds)\"/"
+git ls-files --others --exclude-standard | grep -P '.*\.md$' | xargs --no-run-if-empty sed -i "s/^date = .*/date = \"$(date --iso-8601=seconds)\"/"
